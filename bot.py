0c669afa56777d772e56fd7aa796343a79fcb35e
HellgrenR <rallenolltva@gmail.com>
2024-11-30 22:25:39 +0100

    feat: create class of Bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class Bot(commands.Bot):
+  def __init__(self, prefix ):
+    intents = discord.Intents.default()
+    intents.message_content = True
+    super().__init__(command_prefix=prefix, intents=intents)
+
+    self.add_commands()
+    self.add_events()
+
+  def add_commands(self):
+      @self.command()
+      async def ping(ctx):
+          await ctx.send('pong')
+
+      @self.command()
+      async def hello(ctx):
+          await ctx.send(f"Hello! I'm {self.user}. How can I assist you today?")
+
+  def add_events(self):
+      @self.event
+      async def on_ready():
+          print(f"We have logged in as {self.user}")
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import discord
-from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from bot import Bot
+
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+bot = Bot('!')
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
