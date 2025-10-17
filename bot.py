2bdea58305712ca68b8ee189555e44cfc5ec410c
HellgrenR <rasmus.hellgren1@ingka.ikea.com>
2025-10-17 12:35:42 +0200

    indentation

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import asyncio
 import src.audio_handler as AudioHandler
 import src.url_check as UrlCheck
 
-#fel fixa detta
 class Bot(commands.Bot):
   def __init__(self, prefix ):
     intents = discord.Intents.default()
@@ -18,44 +17,44 @@ class Bot(commands.Bot):
 
 
   def add_commands(self):
-      @self.command()
-      async def play(ctx, *, url: str):
-        # Check if URL is valid
-        if UrlCheck.url_check(url) == False:
-          await ctx.send("Invalid URL")
-          return
+    @self.command()
+    async def play(ctx, *, url: str):
+      # Check if URL is valid
+      if UrlCheck.url_check(url) == False:
+        await ctx.send("Invalid URL")
+        return
 
-        # Check if bot is in voice channel
-        if not self.is_connected(ctx=ctx):
-          await self.join_vc(ctx=ctx)
+      # Check if bot is in voice channel
+      if not self.is_connected(ctx=ctx):
+        await self.join_vc(ctx=ctx)
 
-        # Add audio URL to queue
-        self.music_queue.append(url)
-        await ctx.send("Added to queue")
-        await ctx.send(f"Amount of songs: {len(self.music_queue)}")
+      # Add audio URL to queue
+      self.music_queue.append(url)
+      await ctx.send("Added to queue")
+      await ctx.send(f"Amount of songs: {len(self.music_queue)}")
 
-        # Play audio
-        if not ctx.voice_client.is_playing():
-          await self.play_audio(ctx=ctx)
+      # Play audio
+      if not ctx.voice_client.is_playing():
+        await self.play_audio(ctx=ctx)
 
-      @self.command()
-      async def join(ctx):
-        channel = ctx.author.voice.channel
-        await channel.connect()
+    @self.command()
+    async def join(ctx):
+      channel = ctx.author.voice.channel
+      await channel.connect()
 
-      @self.command()
-      async def stop(ctx):
-        await ctx.voice_client.disconnect()
-        self.music_queue = []
+    @self.command()
+    async def stop(ctx):
+      await ctx.voice_client.disconnect()
+      self.music_queue = []
 
-      @self.command()
-      async def queue(ctx):
-        await ctx.send(len(self.music_queue))
+    @self.command()
+    async def queue(ctx):
+      await ctx.send(len(self.music_queue))
 
   def add_events(self):
-      @self.event
-      async def on_ready():
-        print(f"We have logged in as {self.user}")
+    @self.event
+    async def on_ready():
+      print(f"We have logged in as {self.user}")
   
 
   def is_connected(self, ctx) -> bool:
@@ -81,10 +80,10 @@ class Bot(commands.Bot):
 
       def after_play(error):
         if error:
-            print(f"Error in playback: {error}")
+          print(f"Error in playback: {error}")
         asyncio.run_coroutine_threadsafe(self.play_audio(ctx), self.loop)
 
       ctx.voice_client.play(source, after=after_play)
     else:
       self.music_queue.pop(0)
-      self.play_audio(ctx)
+      await self.play_audio(ctx)
